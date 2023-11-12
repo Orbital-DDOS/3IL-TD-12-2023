d98461318f054c53680dd36b9d50fefc6c66dafb
GreedInc <fabien.escourbiac@gmail.com>
2023-11-12 15:26:48 +0100

    adding parametrized tests + wrong number test

--- lib/src/test/java/com/rodez/demo/FizzBuzzTest.java
+++ lib/src/test/java/com/rodez/demo/FizzBuzzTest.java
@@ -1,8 +1,10 @@
 package com.rodez.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * The {@code FizzBuzzTest} class contains JUnit tests for the {@link FizzBuzz}
@@ -21,51 +23,65 @@ public class FizzBuzzTest {
    * Tests the {@link FizzBuzz#play(int)} method with a number that is not a
    * multiple of 3 or 5. The expected result is the same as the input number
    * converted to a String.
+   * 
+   * @param numberToTest the number to test.
    */
-  @Test
-  public void testPlayWithRegularNumber() {
+  @ParameterizedTest
+  @ValueSource(ints = {1, 7})
+  public void testPlayWithRegularNumber(int numberToTest) {
     FizzBuzz fizzBuzz = new FizzBuzz();
-    assertEquals("1", fizzBuzz.play(1));
-    assertEquals("7", fizzBuzz.play(7));
-    // Add more test cases as needed.
+    assertEquals(String.valueOf(numberToTest), fizzBuzz.play(numberToTest));
   }
 
   /**
    * Tests the {@link FizzBuzz#play(int)} method with a number that is a multiple
    * of 3. The expected result is "Fizz".
+   * 
+   * @param numberToTest the number to test.
    */
-  @Test
-  public void testPlayWithMultipleOfThree() {
+  @ParameterizedTest
+  @ValueSource(ints = {3, 9})
+  public void testPlayWithMultipleOfThree(int numberToTest) {
     FizzBuzz fizzBuzz = new FizzBuzz();
-    assertEquals("Fizz", fizzBuzz.play(3));
-    assertEquals("Fizz", fizzBuzz.play(9));
-    // Add more test cases as needed.
+    assertEquals("Fizz", fizzBuzz.play(numberToTest));
   }
 
   /**
    * Tests the {@link FizzBuzz#play(int)} method with a number that is a multiple
    * of 5. The expected result is "Buzz".
+   * 
+   * @param numberToTest the number to test.
    */
-  @Test
-  public void testPlayWithMultipleOfFive() {
+  @ParameterizedTest
+  @ValueSource(ints = {5, 10})
+  public void testPlayWithMultipleOfFive(int numberToTest) {
     FizzBuzz fizzBuzz = new FizzBuzz();
-    assertEquals("Buzz", fizzBuzz.play(5));
-    assertEquals("Buzz", fizzBuzz.play(10));
-    // Add more test cases as needed.
+    assertEquals("Buzz", fizzBuzz.play(numberToTest));
   }
 
   /**
    * Tests the {@link FizzBuzz#play(int)} method with a number that is a multiple
    * of both 3 and 5. The expected result is "FizzBuzz".
+   * 
+   * @param numberToTest the number to test.
    */
-  @Test
-  public void testPlayWithMultipleOfThreeAndFive() {
+  @ParameterizedTest
+  @ValueSource(ints = {15, 30})
+  public void testPlayWithMultipleOfThreeAndFive(int numberToTest) {
     FizzBuzz fizzBuzz = new FizzBuzz();
-    assertEquals("FizzBuzz", fizzBuzz.play(15));
-    assertEquals("FizzBuzz", fizzBuzz.play(30));
-    // Add more test cases as needed.
+    assertEquals("FizzBuzz", fizzBuzz.play(numberToTest));
   }
-
-  // Add more test cases and methods to cover different scenarios.
-
+  
+  /**
+   * Tests the {@link FizzBuzz#play(int)} method with a number that is lower than 1.
+   * The expected result is an IllegalArgumentException.
+   * 
+   * @param numberToTest the number to test.
+   */
+  @ParameterizedTest
+  @ValueSource(ints = {-15, -1, 0})
+  public void testPlayWithIncorrectNumbers(int numberToTest) {
+    FizzBuzz fizzBuzz = new FizzBuzz();
+    assertThrows(IllegalArgumentException.class, () -> fizzBuzz.play(numberToTest));
+  }
 }
